389dcd5dc596d6cb97b5af7d763ea22e51d8688e
arvinxx <arvinx@foxmail.com>
2023-11-07 03:54:46 +0800

    :recycle: refactor: refactor with new sdk api

--- src/pages/iframe.tsx
+++ src/pages/iframe.tsx
@@ -1,4 +1,4 @@
-import { fetchPluginMessage } from '@lobehub/chat-plugin-sdk/client';
+import { lobeChat } from '@lobehub/chat-plugin-sdk/client';
 import { Skeleton } from 'antd';
 import { memo } from 'react';
 import useSWR from 'swr';
@@ -7,7 +7,7 @@ import Render from '@/Render';
 import { WeatherResult } from '@/type';
 
 const Home = memo(() => {
-  const { isLoading, data } = useSWR<WeatherResult>('fetchDataFromChat', fetchPluginMessage);
+  const { isLoading, data } = useSWR<WeatherResult>('fetchDataFromChat', lobeChat.getPluginMessage);
 
   return isLoading || !data ? <Skeleton active /> : <Render content={data} name={'test'} />;
 });
